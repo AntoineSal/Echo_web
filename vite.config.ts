338a862722ad1a32b07edda341ddb399e15d6f8f
Antoine Salomon <antoinejsalomon@gmail.com>
2026-03-21 19:13:50 +0100

    setup vraiment complet je crois

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mobile': path.resolve(__dirname, './mobile_src'),
+      'react-native': 'react-native-web'
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
